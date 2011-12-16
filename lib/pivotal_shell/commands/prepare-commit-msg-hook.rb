af47bb4b5b7defcbc922ff871f2255487480937c
Leonid Shevtsov <leonid@shevtsov.me>
2011-12-16 15:42:46 +0200

    put commit message before comments

--- lib/pivotal_shell/commands/prepare-commit-msg-hook.rb
+++ lib/pivotal_shell/commands/prepare-commit-msg-hook.rb
@@ -6,10 +6,12 @@ module PivotalShell::Commands
     end
 
     def execute
+      commit_message_lines = @commit_message.split("\n")
+      commit_message_without_comments = commit_message_lines.shift(commit_message_lines.find_index{|line| line =~ /^#/} || commit_message_lines.length)
       File.open(@filename, 'w') do |f|
-        f.puts
+        f.puts commit_message_without_comments.join("\n")
         f.puts stories
-        f.puts @commit_message.lstrip
+        f.puts commit_message_lines.join("\n")
       end
     end
 
