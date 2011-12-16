d199474c958559ce2ba0960da79b86d5c7fa34f1
Leonid Shevtsov <leonid@shevtsov.me>
2011-12-16 15:47:04 +0200

    some refactoring of prepare commit msg

--- lib/pivotal_shell/commands/prepare-commit-msg-hook.rb
+++ lib/pivotal_shell/commands/prepare-commit-msg-hook.rb
@@ -6,13 +6,12 @@ module PivotalShell::Commands
     end
 
     def execute
-      commit_message_lines = @commit_message.split("\n")
-      commit_message_without_comments = commit_message_lines.shift(commit_message_lines.find_index{|line| line =~ /^#/} || commit_message_lines.length)
-      if commit_message_without_comments.join.strip == '' 
+      parse_commit_message
+      if commit_message_empty? 
         File.open(@filename, 'w') do |f|
-          f.puts commit_message_without_comments.join("\n")
+          f.puts @commit_message_without_comments.join("\n")
           f.puts stories
-          f.puts commit_message_lines.join("\n")
+          f.puts @commit_message_lines.join("\n")
         end
       end
     end
@@ -23,6 +22,15 @@ module PivotalShell::Commands
         story_id = "[\##{story.id}]"
         "# #{story_id.rjust(13)} #{story.name}"
       }.join("\n")
+    end
+    
+    def parse_commit_message
+      @commit_message_lines = @commit_message.split("\n")
+      @commit_message_without_comments = @commit_message_lines.shift(@commit_message_lines.find_index{|line| line =~ /^#/} || @commit_message_lines.length)
+    end
+
+    def commit_message_empty?
+      @commit_message_without_comments.join.strip == ''
     end
   end
 end
